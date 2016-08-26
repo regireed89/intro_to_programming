53d86e2dbd68f2fe64cf1a486285290e7274b111
regireed89 <regireed89@gmail.com>
2016-08-26 13:03:00 -0500

    conditionals exercise

---
+++ conditionals excersise/conditionals excersise/Source.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+int main()
+{
+	////1
+	//int y = 0;
+	//if (y == 0)
+	//{
+	//	printf("x = 100");
+	//	system("pause");
+	//	
+
+ //   }
+ //   
+	//{
+	//	//2
+	//	int x = 100;
+	//	int z = 2000;
+
+	//		if (x > z)
+	//		{
+	//			printf("%i", x);
+	//	    }
+	//		else
+	//		{
+	//			printf("%i", z);
+	//		}
+
+	//	system("pause");
+	//	
+
+
+	//}
+
+	////4
+	//switch (choice)
+	//{
+	//case 1:
+	//	std::cout << "1";
+	//	break;
+	//case 2 || 3:
+	//	std::cout << "2 or 3";
+	//	break;
+	//case 4:
+	//	std::cout << "4";
+	//	break;
+	//default:
+	//	break;
+	//}
+	//
+	
+	
+	////5
+	//y = (x == 0) ? 0 : 10 / x
+
+
+		//6
+		/*int a;
+	    int b;
+		
+		std::cout << "enter two numbers and an operation +, -, *, /, or %";
+		std::cin >> a >> operation >> b;
+		{
+			if (operation == +)
+			{
+				int c = a + b;
+				std::cout << c;
+			}
+
+			else if (operation == -)
+			{
+				int c = a - b;
+				std::cout << c;
+			}
+
+			else if (operation == *)
+			{
+				int c = a * b;
+				std::cout << c;
+			}
+
+			else if (operation == /)
+			{
+				int c = a / b;
+				std::cout << c;
+			}
+
+			else if (operation == %)
+			{
+				int c = a % b;
+				std::cout << c;
+			}
+
+			else
+			{
+				std::cout << "da fuck u doin";
+			}
+		}
+		system("pause");
+*/
+
+	/*7
+	int m; 
+	std::cin >> m;
+	
+	if (m == 1)
+	{
+		std::cout << "January has 31 days";
+	}
+	if (m == 2)
+	{
+		std::cout << "February has 28 days";
+	}
+	if (m == 3)
+	{
+		std::cout << "March has 31 days";
+	}
+	if (m == 4)
+	{
+		std::cout << "April has 30 days";
+	}
+	if (m == 5)
+	{
+		std::cout << "May has 31 days";
+	}
+	if (m == 6)
+	{ 
+		std::cout << "June has 30 days";
+	}
+	if (m == 7)
+	{
+		std::cout << "July has 31 days";
+	}
+	if (m == 8)
+	{
+		std::cout << "August has 31 days";
+	}
+	if (m == 9)
+	{
+		std::cout << "September has 30 days";
+	}
+	if (m == 10)
+	{
+		std::cout << "October has 31 days";
+	}
+	if (m == 11)
+
+	{
+	    std::cout << "November has 30 days";
+	}
+	if (m == 12)
+	{
+		std::cout << "December has 31 days";
+	}
+	else if (m >= 13)
+	{
+		std::cout << "not a month stupid";
+	}
+	
+	
+	system("pause");*/
+
+
+
+    //8
+
+    //a. numPos > numNeg = true
+	//b. frstChar > scndChar = true
+	//c. !(numPos + numNeg) = false
+	//d. (numPos == -30) || (numNeg == -55) = true
+	//e. (frstPrice >= 4.1) && (frstPrice <= 9.9) = true
+	//f. !flag && (scndChar <= 'R') = false
+	//g. (numPos < 66) || (flag && numPos > 35) = true
+	//h. ++numPos == 36 = true
+	//i. numPos++ == 36 = false
+	//j. (frstChar == 'j') || (frstChar == 'J') = true
+
+
+	//9
+
+	//a. (a || b) || (a && b) = true
+	//b. !((!a) && (a)) || (a && b) = true
+	//c. !((5 || a) || (!b)) && (!(a) && b) = false
+	//d. a || b && a = false
+	//e. !a&&b = false
+
+
+
+}
